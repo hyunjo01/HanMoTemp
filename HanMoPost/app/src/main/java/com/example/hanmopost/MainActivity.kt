0d5d91427554f0c3811c7da5c1895b849ee1fa4d
hyunjo01 <hyunjo10217@naver.com>
2022-08-08 19:06:59 +0900

    listView 구현 완료 ~

--- HanMoPost/app/src/main/java/com/example/hanmopost/MainActivity.kt
+++ HanMoPost/app/src/main/java/com/example/hanmopost/MainActivity.kt
@@ -5,10 +5,21 @@ import android.os.Bundle
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    var postList = arrayListOf<Post>(
+        Post("A", "Hello", "16:00", "thumbnail1") ,
+        Post("B", "Hi ~~", "17:00", "thumbnail2")
+
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val postAdapter = MainListAdapter(this, postList)
+
+        val postListView: ListView = findViewById(R.id.postListView)
+        postListView.adapter = postAdapter
 
     }
 }
